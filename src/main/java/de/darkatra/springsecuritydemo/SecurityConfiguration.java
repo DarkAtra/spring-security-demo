f97c92dd2ac598222a1ed5c78fbc969e4627fa47
Tommy Schmidt <tommy.schmidt@idealo.de>
2022-07-26 12:13:54 +0200

    chore: spring security without WebSecurityConfigurerAdapter
    * https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter

--- src/main/java/de/darkatra/springsecuritydemo/SecurityConfiguration.java
+++ src/main/java/de/darkatra/springsecuritydemo/SecurityConfiguration.java
@@ -1,19 +1,21 @@
 package de.darkatra.springsecuritydemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
         http.csrf().disable()
             .authorizeRequests().anyRequest().authenticated()
             .and()
             .oauth2ResourceServer().jwt();
+        return http.build();
     }
 }
